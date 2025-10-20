db45bdad2ca6cdedfa0af00d20d67911b74f8ea4
Ingo Schwarz <ingo.schwarz23@gmail.com>
2025-10-20 07:27:48 +0200

    implement args parsing

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,27 @@ mod bencoding;
 use std::path::Path;
 use crate::bencoding::decode_file;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "leech", version, about = "one torrent, no seeding, no bullshit.")]
+struct Cli {
+   /// Print torrent info
+   #[arg(short, long)]
+   debug: bool,
+
+   /// Input file
+   input: String,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-   let result = decode_file(&Path::new("test.torrent"))?;
-   println!("{}", result);
+   let cli = Cli::parse();
+
+   let result = decode_file(&Path::new(cli.input.as_str()))?;
+
+   // if cli.debug {
+      println!("{}", result);
+   // }
 
    Ok(())
 }
